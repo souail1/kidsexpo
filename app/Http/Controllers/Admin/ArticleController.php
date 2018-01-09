0c296fbc1d442ccc5fcb874cd7c69e0ab3a69378
souail1 <706899061@qq.com>
2018-01-09 17:29:31 +0800

    1.9

--- app/Http/Controllers/Admin/ArticleController.php
+++ app/Http/Controllers/Admin/ArticleController.php
@@ -1,38 +1,21 @@
 <?php
 
-namespace App\Http\Controllers\admin;
+namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
-use App\Model\Article;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
 
 class ArticleController extends Controller
 {
-    public function articlesPage()
+    public function __construct()
     {
-        return view('admin.article.list');
+
     }
 
-    public function getArticles(Request $request)
+    public function index()
     {
-        $page = $request['page'];
-        $limit = $request['limit'];
-        $sortfield = $request['sortField'] ?? 'id';
-        $order = $request['order'] ?? 'asc';
-        $offset = ($page - 1) * $limit;
-        $where = ['title', 'like'];
-        $articles = DB::table('articles')
-            ->where($where)
-            ->select('id', 'title', 'author', 'cate_id', 'created_at', 'updated_at')
-            ->offset($offset)
-            ->limit($limit)
-            ->orderBy($sortfield, $order)
-            ->get();
-        $count = count($articles);
-        return [
-            'count' => $count,
-            'data' => $articles
-        ];
+        $articles = DB::table('articles')->get();
+        return view('admin.article.list', ['articles' => $articles]);
     }
 }
--- resources/views/admin/index/index.blade.php
+++ resources/views/admin/index/index.blade.php
@@ -78,7 +78,7 @@
                         </a>
                     </li>
                     <li class="layui-nav-item">
-                        <a href="javascript:;" data-url="/admin/articel">
+                        <a href="javascript:;" data-url="/admin/articel/index">
                             <i class="iconfont icon-text" data-icon="icon-text"></i>
                             <cite>文章列表</cite>
                         </a>
